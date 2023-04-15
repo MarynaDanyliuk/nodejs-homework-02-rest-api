9b3ae71ef728ae53be90200f8b42cf9038f9b75f
Mary Danyliuk <marydanyliuk@gmail.com>
2023-04-15 23:26:30 +0300

    fix errors register and login

--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -4,14 +4,16 @@ const ctrl = require("../../controllers/authController");
 
 const { authenticate } = require("../../middlewares");
 
-const { validateBody } = require("../../helpers");
-const { schemas } = require("../../models/user");
+const {
+  validateRegister,
+  validateLogin,
+} = require("../../validators/authValidate");
 
 const router = express.Router();
 
-router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+router.post("/register", validateRegister, ctrl.register);
 
-router.post("/login", validateBody(schemas.registerSchema), ctrl.login);
+router.post("/login", validateLogin, ctrl.login);
 
 router.get("/current", authenticate, ctrl.getCurrent);
 
